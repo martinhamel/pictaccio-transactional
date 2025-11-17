a7b0547b3262f7fdde40942254fef2c08af1a1f8
Martin Hamel <martin@ma4s.org>
2025-11-16 22:51:44 -0500

    ça marche presque

--- Config/db_vars.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_USERNAME', getenv('DB_USERNAME'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_NAME', getenv('DB_DATABASE_NAME'));
